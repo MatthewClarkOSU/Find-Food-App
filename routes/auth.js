08c2cb04f379d7b446f03c2bdc0b8d5a57f90bd1
MatthewClarkOSU <matthewclarkosu@gmail.com>
2021-10-13 14:52:17 -0400

    added update user functionality

--- routes/auth.js
+++ routes/auth.js
@@ -20,6 +20,7 @@ router.post("/register", async (req, res) => {
         // Save user and respond
         const user = await newUser.save();
         res.status(200).json(user);
+        
     } catch (err) {
         res.status(500).json(err)
     }
@@ -35,6 +36,7 @@ router.post("/login", async (req, res) => {
         !validPassword && res.status(400).json("Wrong password")
 
         res.status(200).json(user)
+
     } catch (err) {
         res.status(500).json(err)
     }
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,36 @@
+const User = require("../models/User")
 const router = require("express").Router();
+const bcrypt = require("bcrypt")
 
-router.get("/",(req,res) => {
-    res.send("this is the user route")
+// Update user
+router.put("/:id", async(req, res) => {
+    if (req.body.userId === req.params.id || req.user.isAdmin) {
+        if (req.body.password) {
+            try {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+
+            } catch(err) {
+                return res.status(500).json(err)
+            }
+        }
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, {
+                $set:req.body,
+            });
+            res.status(200).json("User has been updated")
+        } catch(err) {
+            return res.status(500).json(err)
+        }
+    } else {
+        return res.status(403).json("You do not have access to this account")
+    }
 })
+
+// Delete user
+
+
+// Get user
+
 
 module.exports = router
